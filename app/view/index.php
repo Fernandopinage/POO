e485ddbee63883d0d080b621be752a52e9562dc2
Fernando Pinage <luizfernandoluck@hotmail.com>
2020-02-27 22:46:52 -0300

    ok

--- app/view/index.php
+++ app/view/index.php
@@ -5,30 +5,44 @@ require_once "../controller/controller.php";
 use Controller\controllerFuncionario as Funcionario; // intanciando namespace e acrecentando um apelido
 
 
-// setando o dados manualmente
-$pessoa = new Funcionario();
-$pessoa->setNome('Luiz');
-$pessoa->setTelefone('123456789');
-$pessoa->setCelular('987654321');
-$pessoa->setEmail('Luizfernandoluck@hotmail.com');
+            // setando o dados manualmente
+            $pessoa = new Funcionario();
+            $pessoa->setNome('Luiz');
+            $pessoa->setTelefone('123456789');
+            $pessoa->setCelular('987654321');
+            $pessoa->setEmail('Luizfernandoluck@hotmail.com');
 
 
-echo $pessoa->getNome()."<br>";
-echo $pessoa->getTelefone()."<br>";
-echo $pessoa->getCelular()."<br>";
-echo $pessoa->getEmail()."<br>";
+            echo $pessoa->getNome()."<br>";
+            echo $pessoa->getTelefone()."<br>";
+            echo $pessoa->getCelular()."<br>";
+            echo $pessoa->getEmail()."<br>";
 
-// exemplo de como poderia setar os dados via formulario 
-$nome = 'joao';
-$telefone = '123456789';
-$celular = '987654321';
-$email = 'joao@email.com';
+            // exemplo de como poderia setar os dados via formulario 
+            $nome = 'joao';
+            $telefone = '123456789';
+            $celular = '987654321';
+            $email = 'joao@email.com';
 
-$pessoa->setNome($nome);
-$pessoa->setTelefone($telefone);
-$pessoa->setCelular($celular);
-$pessoa->setEmail($email);
-// enviando os dados para ser inseridos
-$pessoa->insert($nome,$telefone,$celular,$email);
-
+            $pessoa->setNome($nome);
+            $pessoa->setTelefone($telefone);
+            $pessoa->setCelular($celular);
+            $pessoa->setEmail($email);
+            // enviando os dados para ser inseridos
+            $pessoa->insert($nome,$telefone,$celular,$email);
+    
+        
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>TesT</h1>
+</body>
+</html>
